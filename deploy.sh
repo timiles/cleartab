da33ab42577f66a520153f21d5b1c3db4baf1b56
Tim Iles <tim@timiles.com>
2023-08-22 11:22:47 +0100

    Deploy script to take semver level as arg

--- deploy.sh
+++ deploy.sh
@@ -7,16 +7,20 @@ if ! [ -z "$(git status --porcelain)" ]; then
   exit 1
 fi
 
+# Read semver level (major, minor, patch), default to minor
+semverLevel=${1:-minor}
+
 # Build
 npm run build
 
 # Commit
-version=`npx next-standard-version --releaseAs minor`
+version=`npx next-standard-version --releaseAs $semverLevel`
+
 git add .
 git commit -m "Build version $version"
 
 # Tag
-npm version minor
+npm version $semverLevel
 
 # Push
 git push --follow-tags
